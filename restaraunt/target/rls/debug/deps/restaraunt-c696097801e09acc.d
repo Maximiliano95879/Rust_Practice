da492561ce59a5200cd682e5ccb94fce5ed2b4b9
Maximiliano Robledo <maxrobledo13@gmail.com>
2021-01-04 12:13:20 -0600

    module and crate practice

---
+++ restaraunt/target/rls/debug/deps/restaraunt-c696097801e09acc.d
@@ -0,0 +1,5 @@
+/home/maximiliano/Rust_Projects/restaraunt/target/rls/debug/deps/restaraunt-c696097801e09acc.rmeta: src/lib.rs
+
+/home/maximiliano/Rust_Projects/restaraunt/target/rls/debug/deps/restaraunt-c696097801e09acc.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaraunt/target/rls/debug/deps/restaraunt-db3d21a313d16961.d
@@ -0,0 +1,5 @@
+/home/maximiliano/Rust_Projects/restaraunt/target/rls/debug/deps/restaraunt-db3d21a313d16961.rmeta: src/lib.rs
+
+/home/maximiliano/Rust_Projects/restaraunt/target/rls/debug/deps/restaraunt-db3d21a313d16961.d: src/lib.rs
+
+src/lib.rs:
