205c0fe72d205d296b8e4844198364426dcdf99b
Maximiliano Robledo <maxrobledo13@gmail.com>
2021-01-05 15:49:05 -0600

    messing around with trai/interface thing

---
+++ traits/target/rls/debug/deps/traits-5c98123b7269a78d.d
@@ -0,0 +1,5 @@
+/home/maximiliano/Rust_Projects/traits/target/rls/debug/deps/traits-5c98123b7269a78d.rmeta: src/lib.rs
+
+/home/maximiliano/Rust_Projects/traits/target/rls/debug/deps/traits-5c98123b7269a78d.d: src/lib.rs
+
+src/lib.rs:
---
+++ traits/target/rls/debug/deps/traits-a285f8cff7fb5937.d
@@ -0,0 +1,5 @@
+/home/maximiliano/Rust_Projects/traits/target/rls/debug/deps/traits-a285f8cff7fb5937.rmeta: src/lib.rs
+
+/home/maximiliano/Rust_Projects/traits/target/rls/debug/deps/traits-a285f8cff7fb5937.d: src/lib.rs
+
+src/lib.rs:
