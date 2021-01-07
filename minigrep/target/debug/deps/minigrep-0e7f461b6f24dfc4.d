d376d8e51b4e941aac29f2c75bf046b20192454a
Maximiliano Robledo <maxrobledo13@gmail.com>
2021-01-06 20:26:30 -0600

    added some test and functions, refactored

---
+++ minigrep/target/debug/deps/minigrep-0e7f461b6f24dfc4.d
@@ -0,0 +1,5 @@
+/home/maximiliano/Rust_Projects/minigrep/target/debug/deps/minigrep-0e7f461b6f24dfc4: src/lib.rs
+
+/home/maximiliano/Rust_Projects/minigrep/target/debug/deps/minigrep-0e7f461b6f24dfc4.d: src/lib.rs
+
+src/lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-dc07370efc05a1f3.d
@@ -0,0 +1,5 @@
+/home/maximiliano/Rust_Projects/minigrep/target/debug/deps/minigrep-dc07370efc05a1f3: src/main.rs
+
+/home/maximiliano/Rust_Projects/minigrep/target/debug/deps/minigrep-dc07370efc05a1f3.d: src/main.rs
+
+src/main.rs:
