572b7fe681899b2f5602289416056604d9f5cabe
Maximiliano Robledo <maxrobledo13@gmail.com>
2021-01-06 23:29:15 -0600

    closure practice

---
+++ closures/target/rls/debug/deps/closures-b8c3bbae92a630cb.d
@@ -0,0 +1,5 @@
+/home/maximiliano/Rust_Projects/closures/target/rls/debug/deps/closures-b8c3bbae92a630cb.rmeta: src/main.rs
+
+/home/maximiliano/Rust_Projects/closures/target/rls/debug/deps/closures-b8c3bbae92a630cb.d: src/main.rs
+
+src/main.rs:
